0f6ddebb47fd99c8331e54f5de2214a426d36943
Taylor Edwards <me@tayloredwards.net>
2017-09-24 12:05:30 -0700

    Add support for namespaced scopes

--- README.md
+++ README.md
@@ -1,16 +1,41 @@
 # Publish-Subscribe in JS
 
-This ECMAScript 2015 module implements the publish-subscribe pattern for
-use in client side applications.
+This ECMAScript 2015 module implements the publish-subscribe pattern
+for use in client side applications.
 
 ## Usage
+
+PubSub in global scope:
 ```JavaScript
 import { publish, subscribe } from 'pubsub';
 
-subscribe('myEventName', (value) => {
+let sub = subscribe('myEventName', value => {
   console.log(`Hello, ${value}!`);
 });
 
 publish('myEventName', 'world');
 // Hello, world!
+
+sub.remove();
+publish('myEventName', 'you');
+// nothing happens
 ```
+
+Namespaced scopes:
+```JavaScript
+import pubsub from 'pubsub';
+let myObj = {};
+pubsub(myObj);
+
+let mySub = myObj.pubsub.subscribe('foo', () => console.log('namespaced foo'));
+myObj.pubsub.publish('foo');
+// 'namespaced foo'
+
+mySub.remove();
+myObj.pubsub.publish('foo');
+// nothing happens
+```
+
+This implementation also supports an arbitrary number of arguments.
+Whatever additional arguments you pass to the `publish` method will
+be forwarded to the callbacks provided with the `subscribe` method.
--- pubsub.js
+++ pubsub.js
@@ -1,31 +1,45 @@
-const topics = {};
+const globalTopics = {};
 
 const nextId = (() => {
   let i = 0;
-  return () => i++;
+  return () => (i++).toString(16);
 })();
 
 export function subscribe(topic, callback) {
+  const topics = this.pubsub && this.pubsub._topics ? this.pubsub._topics : globalTopics;
   if (!topics.hasOwnProperty(topic)) {
     topics[topic] = {};
   }
 
   const id = nextId();
-
   topics[topic][id] = callback;
 
   return {
-    remove: () => {
-      delete topics[topic][id];
-    }
+    remove: () => delete topics[topic][id]
   };
 }
 
 export function publish(topic) {
+  const topics = this.pubsub && this.pubsub._topics ? this.pubsub._topics : globalTopics;
   if (topics.hasOwnProperty(topic)) {
     const args = [...arguments].splice(1);
+
     Object.keys(topics[topic]).forEach(key => 
       topics[topic][key].apply(undefined, args)
     );
+  }
+}
+
+export default function pubsub(root) {
+  // Check if any of the properties already exist:
+  // 1) Makes this function idempotent
+  // 2) Prevents overriding other properties
+
+  if (!root.hasOwnProperty('pubsub')) {
+    root.pubsub = {
+      _topics: {},
+      subscribe: subscribe.bind(root),
+      publish: publish.bind(root),
+    };
   }
 }
